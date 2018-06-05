72abaab0cd7e7179e3ae67bf57a026027050da98
Leonardo Kamache <lkamache@Leos-MacBook-Pro.local>
2018-06-05 16:52:53 -0300

    Primeiro commit

---
+++ nome.py
@@ -0,0 +1,11 @@
+nome = raw_input("Whats is your name? ")
+idade = input("What is your age? ")
+
+if idade < 2:
+	blau = " year"
+else:
+	blau = " years"
+
+
+print("")
+print("Your name is "+nome+" and you are "+str(idade)+blau+" old")
